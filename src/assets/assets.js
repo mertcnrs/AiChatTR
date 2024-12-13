b8a8831d5f6826a9ba8036a5e7cb408e7bff0171
Mert Çinar <mertcnr15@gmail.com>
2024-12-13 23:17:04 +0300

    Update assets.js

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -10,7 +10,7 @@ import mic_icon from './mic_icon.png'
 import user_icon from './user_icon.png'
 import user from './user.jpg'
 import youtube_icon from './youtube_icon.png'
-import message_icon from './message_icon.png'
+import messagee_icon from './messagee_icon.png'
 import code_icon from './code_icon.png'
 import send_icon from './send_icon.png'
 import gemini_icon from './gemini_icon.png'
@@ -27,7 +27,7 @@ export const assets = {
 	mic_icon,
 	user_icon,
 	youtube_icon,
-	message_icon,
+	messagee_icon,
 	code_icon,
 	send_icon,
 	gemini_icon,
